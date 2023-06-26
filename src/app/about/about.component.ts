958e960ead6247a86195de3c35d81ffc7dd5cb5f
FiniteLooper <chris.m.barr@gmail.com>
2023-06-26 15:10:21 -0400

    icon animations

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent {
+  skillAnimationTriggered = false;
+  @ViewChild('skillsIconsContainer', { static: true }) skillsIconsContainer!: ElementRef;
 
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(): void {
+    if (!this.skillAnimationTriggered) {
+      const iconsEl = this.skillsIconsContainer.nativeElement as HTMLElement;
+      const iconsTop = iconsEl.getBoundingClientRect().top;
+      if (window.innerHeight >= iconsTop + 100) {
+        this.skillAnimationTriggered = true;
+        iconsEl.classList.add('animated');
+      }
+    }
+  }
 }
