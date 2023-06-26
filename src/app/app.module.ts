e40a7f4bf436a01bb86ce6f4f699fdb16de37521
FiniteLooper <chris.m.barr@gmail.com>
2023-06-26 13:41:14 -0400

    Contact page just has icons for now. Will work on form later

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,10 +10,19 @@ import { DevComponent } from './dev/dev.component';
 import { DesignComponent } from './design/design.component';
 import { PhotoComponent } from './photo/photo.component';
 import { SvgIconComponent } from './about/svg-icon/svg-icon.component';
+import { ContactFormComponent } from './about/contact-form/contact-form.component';
 
 @NgModule({
-  declarations: [AppComponent, AboutComponent, DevComponent, DesignComponent, PhotoComponent, SvgIconComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    DevComponent,
+    DesignComponent,
+    PhotoComponent,
+    SvgIconComponent,
+    ContactFormComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
 })
