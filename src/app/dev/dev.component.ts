ca78ac0ae3bc53b57d5625f56a821f794d5f5be9
FiniteLooper <chris.m.barr@gmail.com>
2023-06-27 12:15:39 -0400

    Move portfolio related styles to where they can be shared

--- src/app/dev/dev.component.ts
+++ src/app/dev/dev.component.ts
@@ -1,10 +1,9 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dev',
   templateUrl: './dev.component.html',
   styleUrls: ['./dev.component.scss'],
-  encapsulation: ViewEncapsulation.None
 })
 export class DevComponent {
 
