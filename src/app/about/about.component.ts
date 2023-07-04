d6d9435f22ab759dca8a43b3b3c2e9200095ab0b
FiniteLooper <chris.m.barr@gmail.com>
2023-07-03 21:26:23 -0400

    Skills now generated from json data. less stuff in the template

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,23 +1,26 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, ElementRef, HostListener, Inject, ViewChild, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, Inject, ViewChild, ViewChildren, QueryList, AfterViewInit } from '@angular/core';
+import { ISkillGroup, skillGroups } from '../data/skills.data';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements AfterViewInit {
   private readonly window: Window = this.document.defaultView as Window;
   @ViewChild('introContainer', { static: true }) private introContainer!: ElementRef<HTMLElement>;
   @ViewChild('skillsContainer', { static: true }) private skillsContainer!: ElementRef<HTMLElement>;
-  @ViewChild('firstSkillsIconsCard', { static: true }) private firstSkillsIconsCard!: ElementRef<HTMLElement>;
+  @ViewChildren('skillsIconsCard') private skillsIconsCard!: QueryList<ElementRef<HTMLElement>>;
 
   private arrowTimer?: ReturnType<typeof setTimeout>;
   private skillAnimationTriggered = false;
 
+  skillGroups: ISkillGroup[] = skillGroups;
+
   constructor(@Inject(DOCUMENT) private readonly document: Document) {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     //prevent a build error for SSR
     if (typeof getComputedStyle === 'function') {
       //Only show the arrow animation if the intro area is smaller than it's max height
@@ -26,7 +29,7 @@ export class AboutComponent implements OnInit {
       const maxHeight = parseInt(getComputedStyle(this.introContainer.nativeElement).getPropertyValue('max-height'), 10);
       const actualHeight = this.introContainer.nativeElement.getBoundingClientRect().height;
 
-      this.onWindowScroll();//manually trigger initially
+      this.onWindowScroll(); //manually trigger initially
       if (actualHeight <= maxHeight) {
         this.arrowTimer = setTimeout(() => {
           this.introContainer.nativeElement.classList.add('show-arrow');
@@ -44,7 +47,7 @@ export class AboutComponent implements OnInit {
     }
 
     if (!this.skillAnimationTriggered) {
-      const iconsTop = this.firstSkillsIconsCard.nativeElement.getBoundingClientRect().top;
+      const iconsTop = this.skillsIconsCard.first.nativeElement.getBoundingClientRect().top;
       if (this.window.innerHeight >= iconsTop + 100) {
         this.skillAnimationTriggered = true;
         this.skillsContainer.nativeElement.classList.add('skills-icons-animate');
---
+++ src/app/data/skills.data.ts
@@ -0,0 +1,54 @@
+export interface ISkillGroup {
+  groupName: string;
+  skillList: ISkill[];
+}
+
+export interface ISkill {
+  iconId: string;
+  skillName: string;
+}
+
+export const skillGroups: ISkillGroup[] = [
+  {
+    groupName: 'Languages',
+    skillList: [
+      { iconId: 'typescript', skillName: 'TypeScript 5' },
+      { iconId: 'javascript', skillName: 'Javascript' },
+      { iconId: 'css3', skillName: 'CSS 3' },
+      { iconId: 'sass', skillName: 'SCSS/SASS' },
+      { iconId: 'html5', skillName: 'HTML 5' },
+    ],
+  },
+  {
+    groupName: 'Frameworks',
+    skillList: [
+      { iconId: 'angular', skillName: 'Angular 16' },
+      { iconId: 'dotnet', skillName: '.NET Core/MVC' },
+      { iconId: 'node', skillName: 'Node.js' },
+      { iconId: 'npm', skillName: 'NPM' },
+      { iconId: 'bootstrap', skillName: 'Bootstrap' },
+      { iconId: 'tailwind', skillName: 'Tailwind CSS' },
+      { iconId: 'rxjs', skillName: 'RXJS' },
+      { iconId: 'eslint', skillName: 'ES Lint' },
+      { iconId: 'gulp', skillName: 'Gulp' },
+      { iconId: 'jest', skillName: 'Jest' },
+      { iconId: 'jasmine', skillName: 'Jasmine' },
+      { iconId: 'karma', skillName: 'Karma' },
+      { iconId: 'jquery', skillName: 'jQuery' },
+    ],
+  },
+  {
+    groupName: 'Software',
+    skillList: [
+      { iconId: 'git', skillName: 'GIT' },
+      { iconId: 'vscode', skillName: 'VS Code' },
+      { iconId: 'visualstudio', skillName: 'Visual Studio' },
+      { iconId: 'photoshop', skillName: 'Photoshop' },
+      { iconId: 'illustrator', skillName: 'Illustrator' },
+      { iconId: 'xd', skillName: 'Adobe XD' },
+      { iconId: 'lightroom', skillName: 'Lightroom' },
+      { iconId: 'jira', skillName: 'Jira' },
+      { iconId: 'trello', skillName: 'Trello' },
+    ],
+  },
+];
