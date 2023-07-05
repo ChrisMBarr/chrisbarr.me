987aedc322b520e1c40dae0070219c121a5688bb
FiniteLooper <chris.m.barr@gmail.com>
2023-07-05 12:56:19 -0400

    Adding portfolio site into my portfolio!

--- src/app/data/dev.data.ts
+++ src/app/data/dev.data.ts
@@ -15,7 +15,7 @@ export const devProjectList: IDevProject[] = [
       images: [
         new CustomImageItem({
           src: 'https://github.com/FiniteLooper/LyricConverter/raw/master/lyric-converter-demo.gif?raw=true',
-          thumb: 'https://github.com/FiniteLooper/LyricConverter/raw/master/lyric-converter-demo.gif?raw=true',
+          thumb: '/assets/dev/lyric-converter_Thumb.png',
           alt: 'LyricConverter Screen Recording',
           thumbTitle: 'LyricConverter Screen Recording'
         }),
@@ -35,7 +35,38 @@ export const devProjectList: IDevProject[] = [
       },
     ],
     beginDescription: `<p><strong>LyricConverter</strong> is a project I've built and improved, on and off, for years. It was made to extract song data and lyrics from many popular lyric display software packages and convert them to other formats. Many of these software packages do not have great import/export tools so I wanted to build something to help people keep their song libraries as they migrated to new software. </p>`,
-    endDescription: `<p>LyricConverter will take raw file input by dragging files into the browser or manually choosing files, extract all relevant song information from <strong>8 different input file formats</strong> to get them into a standard format used internally. Then the songs are converted into one of the <strong>6 possible output formats</strong> the user can select, or they can simply view the song information right there on the site. If a file output format is chosen, the user can choose to download the files as a compressed .zip file or as individual files. </p><p> All of the file parsing, extraction, creation, and saving takes place client side within the Angular app nearly instantaneously, even for hundreds of songs at once! </p><p> Originally written in PHP many years ago, I rewrote it all with client side JavaScript libraries. In recent years this code became cumbersome and difficult to manage or improve upon. I recently completely rewrote it in TypeScript with Angular 16, and it is now extremely maintainable and easier to improve upon. <strong>It has 200+ unit tests to ensure code quality with over 99% code coverage</strong>. Code linting rules are enforced, and github actions will enforce these code quality tools are run when anyone makes a pull request.</p><p>If you'd like to see more images and read about the UI design you can <a href="/design/lyric-converter" rel="ng">see it in the design section</a>.</p>`,
+    endDescription: `<p>LyricConverter will take raw file input by dragging files into the browser or manually choosing files, extract all relevant song information from <strong>8 different input file formats</strong> to get them into a standard format used internally. Then the songs are converted into one of the <strong>6 possible output formats</strong> the user can select, or they can simply view the song information right there on the site. If a file output format is chosen, the user can choose to download the files as a compressed .zip file or as individual files. </p><p> All of the file parsing, extraction, creation, and saving takes place client side within the Angular app nearly instantaneously, even for hundreds of songs at once! </p><p> Originally written in PHP many years ago, I rewrote it all with client side JavaScript libraries. In recent years this code became cumbersome and difficult to manage or improve upon. I recently completely rewrote it in TypeScript with Angular 16, and it is now extremely maintainable and easier to improve upon. <strong>It has 200+ unit tests to ensure code quality with over 99% code coverage</strong>. Code linting rules are enforced, and github actions will enforce these code quality tools are run when anyone makes a pull request.</p><p>It also has as CI/CD deployment script on github so that any changes pushed from git are automatically built and deployed to this live site.  Since it's only a javascript single-page-app it is just hosted on Github pages as it does not require any server-side resources.</p><p>If you'd like to see more images and read about the UI design you can <a href="/design/lyric-converter" rel="ng">see it in the design section</a>.</p>`,
+  },
+  {
+    title: 'Portfolio Website',
+    urlSlug: 'portfolio-site',
+    gallery: {
+      id: 'portfolio',
+      images: [
+        new CustomImageItem({
+          src: '/assets/dev/portfolio-site-desktop.gif',
+          thumb: '/assets/dev/portfolio-site-desktop_Thumb.png',
+          alt: 'Portfolio Website - Desktop Screen Size',
+          thumbTitle: 'Desktop Screen Size'
+        }),
+        new CustomImageItem({
+          src: '/assets/dev/portfolio-site-mobile.gif',
+          thumb: '/assets/dev/portfolio-site-mobile_Thumb.png',
+          alt: 'Portfolio Website - Mobile Screen Size',
+          thumbTitle: 'Mobile Screen Size'
+        }),
+      ],
+    },
+    techStack: 'TypeScript 5, SCSS, Angular 16, Angular Universal',
+    links: [
+      {
+        url: 'https://github.com/FiniteLooper/chrisbarr.me',
+        text: 'Source Code',
+        iconId: 'github',
+      },
+    ],
+    beginDescription: `<p>This website you are looking at right now is my portfolio, and it is also a piece of my development portfolio! It's built using Angular 16 and makes use of Angular Universal to prerender all the pages to make the app load quicker and make it more SEO friendly.</p>`,
+    endDescription: `<p>This portfolio site makes use of SVG icons, and an SVG grain/noise filter on the background to give it a "grainy film" look. It also has as CI/CD deployment script on github so that any changes pushed from git are automatically built and deployed to this live site you are seeing now, which includes pre-rendering each individual route.</p>`,
   },
   {
     title: 'Song Lyric File Format Parsing Libraries',
