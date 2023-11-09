47a9d7ae98abd79ca0bbce03f55406ddda914515
FiniteLooper <chris.m.barr@gmail.com>
2023-11-09 09:57:35 -0500

    Update cleanup & fixes for SSR

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Visit it at: https://chrisbarr.me
 
 This is the source code for my portfolio website.
-It's built using Angular 16, with Angular Universal prerendering to help out with SEO
+It's built using Angular 17, with Angular Universal prerendering to help out with SEO
 
 
 # TODO
--- server.ts
+++ server.ts
@@ -1,4 +1,3 @@
-
 import 'zone.js/node';
 
 import { APP_BASE_HREF } from '@angular/common';
@@ -6,7 +5,7 @@ import { CommonEngine } from '@angular/ssr';
 import * as express from 'express';
 import { existsSync } from 'node:fs';
 import { join } from 'node:path';
-import bootstrap from './src/main.server';
+import { AppServerModule } from './src/main.server';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
@@ -24,9 +23,12 @@ export function app(): express.Express {
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
-  server.get('*.*', express.static(distFolder, {
-    maxAge: '1y'
-  }));
+  server.get(
+    '*.*',
+    express.static(distFolder, {
+      maxAge: '1y',
+    })
+  );
 
   // All regular routes use the Angular engine
   server.get('*', (req, res, next) => {
@@ -34,12 +36,11 @@ export function app(): express.Express {
 
     commonEngine
       .render({
-        bootstrap,
+        bootstrap: AppServerModule,
         documentFilePath: indexHtml,
         url: `${protocol}://${headers.host}${originalUrl}`,
         publicPath: distFolder,
-        providers: [
-          { provide: APP_BASE_HREF, useValue: baseUrl },],
+        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
       })
       .then((html) => res.send(html))
       .catch((err) => next(err));
@@ -63,9 +64,9 @@ function run(): void {
 // The below code is to ensure that the server is run only when not requiring the bundle.
 declare const __non_webpack_require__: NodeRequire;
 const mainModule = __non_webpack_require__.main;
-const moduleFilename = mainModule && mainModule.filename || '';
+const moduleFilename = (mainModule && mainModule.filename) || '';
 if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
   run();
 }
 
-export default bootstrap;
+export default AppServerModule;
