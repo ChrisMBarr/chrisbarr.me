651bdaf6a9580393d3780cf6d91d2c8fd1dc9b38
Chris Barr <chris.m.barr@gmail.com>
2024-06-01 15:35:44 -0400

    Replace Google Analytics with MS Clarity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
-import { NgModule, isDevMode } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 //3rd party modules
-import { NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule } from 'ngx-google-analytics';
 import { LightboxModule } from 'ng-gallery/lightbox';
 import { GalleryModule } from 'ng-gallery';
 
@@ -51,8 +50,6 @@ import { SkillGroupCardComponent } from './home/skill-group-card/skill-group-car
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NgxGoogleAnalyticsModule.forRoot(isDevMode() ? '' : 'G-XBQPNRYEHJ'),
-    NgxGoogleAnalyticsRouterModule,
     GalleryModule,
     LightboxModule,
     FormsModule,
